a41c04468c6530f9d920e676cf6ae29ceaecab83
Wilkor <wilkorguitarrista@msn.com>
2020-03-11 17:59:24 +0000

    ajuste

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,7 +47,7 @@ class Home extends Component {
                   deleteResources(element)
                   }, x*index);
 
-                  })(0.8);
+                  })(1);
               x++;
      })
          alert("Regras Deletadas")
@@ -83,7 +83,7 @@ class Home extends Component {
 
           },x*index);
 
-          })(0.8);
+          })(1);
 
      }
 
